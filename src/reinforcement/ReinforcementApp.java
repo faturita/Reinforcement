837901e27752a13feb51b09851bab62d80fd7d9d
faturita <faturita@kasei>
2016-07-14 23:02:47 -0300

    Added code files to implement a simple q learning policy

---
+++ src/reinforcement/ReinforcementApp.java
@@ -0,0 +1,44 @@
+/*
+ * ReinforcementApp.java
+ */
+
+package reinforcement;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class ReinforcementApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new ReinforcementView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of ReinforcementApp
+     */
+    public static ReinforcementApp getApplication() {
+        return Application.getInstance(ReinforcementApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(ReinforcementApp.class, args);
+    }
+}
